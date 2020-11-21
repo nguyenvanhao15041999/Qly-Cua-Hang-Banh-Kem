b6a0230235f4e431b6c48f058e662df995d3b863
Hao Van <34046312+nguyenvanhao15041999@users.noreply.github.com>
2020-11-21 08:01:26 +0700

    Hao update
    Update navigation drawer quanlynhanvien

--- ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/signin/SignInFragment.java
+++ ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/signin/SignInFragment.java
@@ -1,9 +1,11 @@
 package com.example.projectchuyende.ui.signin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,10 +14,21 @@ import androidx.fragment.app.Fragment;
 import com.example.projectchuyende.R;
 
 public class SignInFragment extends Fragment {
+    Button btnSignUp;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_signin, container, false);
+        btnSignUp = root.findViewById(R.id.btnSignup);
         return root;
+//        btnSignUp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(v.getContext(), .class);
+//                v.getContext().startActivity(intent);
+//                getActivity().finish();
+//
+//            }
+//        });
     }
 }
