e32a4485cd911b637a164dc5c046d8549b77b2f4
Hao Van <34046312+nguyenvanhao15041999@users.noreply.github.com>
2020-11-26 10:54:51 +0700

    Hao update
    Update signIn, Signup

--- ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/signin/SignInFragment.java
+++ ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/signin/SignInFragment.java
@@ -1,6 +1,5 @@
 package com.example.projectchuyende.ui.signin;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,25 +9,32 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.projectchuyende.R;
+import com.example.projectchuyende.ui.signup.SignUpFragment;
 
 public class SignInFragment extends Fragment {
-    Button btnSignUp;
+    Button btnSignup;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_signin, container, false);
-        btnSignUp = root.findViewById(R.id.btnSignup);
+        btnSignup = root.findViewById(R.id.btnSignup);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new SignUpFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                fragmentTransaction.replace(R.id.content_frame, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
         return root;
-//        btnSignUp.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(v.getContext(), .class);
-//                v.getContext().startActivity(intent);
-//                getActivity().finish();
-//
-//            }
-//        });
     }
+
 }
--- ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/signup/SignUpFragment.java
+++ ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/signup/SignUpFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.example.projectchuyende.R;
@@ -16,6 +17,8 @@ public class SignUpFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_signup, container, false);
+        Toolbar toolbar = root.findViewById(R.id.toolbar_Signup);
+
 return root;
     }
 }
