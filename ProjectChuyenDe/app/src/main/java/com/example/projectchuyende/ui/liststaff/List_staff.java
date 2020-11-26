192801483755aeb7dc6547df09848ab2b8320ecd
phanduythai <33258533+phanduythai@users.noreply.github.com>
2020-11-26 07:18:28 +0700

    update_ver2.1
    changes

--- ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/liststaff/List_staff.java
+++ ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/liststaff/List_staff.java
@@ -2,15 +2,19 @@ package com.example.projectchuyende.ui.liststaff;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.projectchuyende.R;
 import com.example.projectchuyende.adapter.ListStaffAdapter;
@@ -26,14 +30,30 @@ public class List_staff extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.show_liststaff, container, false);
+        View root = inflater.inflate(R.layout.activity_list_staff, container, false);
         lv_listStaff = root.findViewById(R.id.lv_listStaff);
+        setEvent();
         return root;
+
     }
     private void setEvent() {
         dataViewListStaff();
         ListStaffadapter = new ListStaffAdapter(getContext(), R.layout.show_liststaff, datalistStaff);
         lv_listStaff.setAdapter(ListStaffadapter);
+        lv_listStaff.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+                builder.setMessage("Ban co muon xoa?");
+                builder.setTitle("Chuc nang");
+                builder.setPositiveButton("Xoa", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getContext(),"ban xoa thanh cong",Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
     }
 
     private void dataViewListStaff() {
