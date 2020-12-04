76a2b7c5757986441a9784f6cb89f43dbff3e4ee
Vu12345-vu <67858025+Vu12345-vu@users.noreply.github.com>
2020-12-04 07:26:39 +0700

    layout
    layout_chitiet
    layout_suataikhoan

---
+++ ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/account/Account.java
@@ -0,0 +1,16 @@
+package com.example.projectchuyende.ui.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projectchuyende.R;
+
+public class Account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
---
+++ ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/account/Change_account.java
@@ -0,0 +1,63 @@
+package com.example.projectchuyende.ui.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.projectchuyende.R;
+import com.example.projectchuyende.model.Nhanvien;
+
+public class Change_account extends AppCompatActivity {
+    Button btn_taotaikhoan;
+    EditText edtManv, edtTennv, edtsdt, edtmatkhau, edtTaikhoan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_account);
+        btn_taotaikhoan = findViewById(R.id.btnTao);
+        edtManv = findViewById(R.id.txtmanv);
+        edtTennv = findViewById(R.id.txtnamenv);
+        edtsdt = findViewById(R.id.txtsdtnv);
+        edtTaikhoan = findViewById(R.id.txtAccount);
+        edtmatkhau = findViewById(R.id.txtpass);
+        btn_taotaikhoan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Nhanvien nhanvien = new Nhanvien();
+                if (nhanvien != null) {
+                    if (edtManv.length() == 0) {
+                        Toast.makeText(Change_account.this, "Vui lòng nhập mã nhân viên", Toast.LENGTH_SHORT).show();
+                    } else if (edtTennv.length() == 0) {
+                        Toast.makeText(Change_account.this, "Vui lòng nhập tên nhân viên", Toast.LENGTH_SHORT).show();
+                    } else if (edtsdt.length() == 0) {
+                        Toast.makeText(Change_account.this, "Vui lòng nhập số điện thoại", Toast.LENGTH_SHORT).show();
+                    } else if (edtTaikhoan.length() == 0) {
+                        Toast.makeText(Change_account.this, "Vui lòng nhập tài khoản", Toast.LENGTH_SHORT).show();
+                    } else if (edtmatkhau.length() == 0) {
+                        Toast.makeText(Change_account.this, "Vui lòng nhập mật khẩu", Toast.LENGTH_SHORT).show();
+                    } else if (edtManv.length() < 5 || edtTennv.length() < 5 || edtsdt.length() < 5 || edtTaikhoan.length() < 5 || edtmatkhau.length() < 5) {
+                        Toast.makeText(Change_account.this, "Điền ít nhất 5 ký tự cho tất cả các trường!!!", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }
+
+        });
+    }
+
+//    private Nhanvien createNhanvien() {
+//        String manv = edtManv.getText().toString();
+//        String tennv = edtTennv.getText().toString();
+//        String sdt = edtsdt.getText().toString();
+//        String taikhoan = edtTaikhoan.getText().toString();
+//        String password = edtmatkhau.getText().toString();
+//        // Nhanvien nhanvien = new Nhanvien("",manv, tennv, sdt,taikhoan, password);
+//        //return nhanvien;
+//    }
+}
