440bb1e80ab5dd2e84533974853d050d796e5743
Trần Minh Thuận <32482005+songtu1307@users.noreply.github.com>
2020-12-10 09:44:23 +0700

    Layout
    xong form layout

--- ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/home/HomeFragment.java
+++ ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/home/HomeFragment.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import com.example.projectchuyende.R;
 import com.example.projectchuyende.ui.order.BookParty;
 import com.example.projectchuyende.ui.order.Bookdesk;
+import com.example.projectchuyende.ui.table.Table;
 
 public class HomeFragment extends Fragment {
     Button btnKhu, btnDatTiec;
@@ -29,7 +30,7 @@ public class HomeFragment extends Fragment {
         btnKhu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), Bookdesk.class);
+                Intent intent = new Intent(getActivity(), Table.class);
                 getActivity().startActivity(intent);
             }
         });
--- ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/table/Table.java
+++ ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/table/Table.java
@@ -1,4 +1,4 @@
-package com.example.projectchuyende.ui.order;
+package com.example.projectchuyende.ui.table;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,36 +10,36 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.projectchuyende.R;
-import com.example.projectchuyende.ui.bill.Bill;
+import com.example.projectchuyende.ui.order.BookParty;
+import com.example.projectchuyende.ui.order.Bookdesk;
 
-public class BookParty extends AppCompatActivity {
+public class Table extends AppCompatActivity {
     ImageView imgBack;
-    Button btnDat, btnHuy;
+    Button btnDatBan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bookparty);
-        Toolbar toolbar = findViewById(R.id.toolbarbookdesk);
+        setContentView(R.layout.activity_table);
+        Toolbar toolbar = findViewById(R.id.toolbartable);
         imgBack= findViewById(R.id.imgBack);
-        btnDat= findViewById(R.id.btnDat);
-        btnHuy= findViewById(R.id.btnHuy);
+        btnDatBan = findViewById(R.id.btnDatBan);
         setSupportActionBar(toolbar);
         setEvent();
     }
 
     public void setEvent() {
+        btnDatBan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Table.this, Bookdesk.class);
+                startActivity(intent);
+            }
+        });
+
         imgBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
-            }
-        });
-
-        btnDat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(BookParty.this, Bill.class);
-                startActivity(intent);
             }
         });
     }
