3628a8a9bfd9d60c7377498a79f9c30092f25586
Trần Minh Thuận <32482005+songtu1307@users.noreply.github.com>
2020-12-24 07:29:48 +0700

    update

--- ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/home/HomeFragment.java
+++ ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/home/HomeFragment.java
@@ -49,25 +49,15 @@ public class HomeFragment extends Fragment {
         btnNuocUong = root.findViewById(R.id.btnNuoc);
         lvDanhSach = root.findViewById(R.id.lvDanhsachHome);
 
-        //lvDanhSach.setOnItemLongClickListener(new ItemLongClickRemove());
-        setEvent();
         FirebaseBanh = new FirebaseBanh(getActivity());
         FirebaseNuoc = new FirebaseNuoc(getActivity());
         setEvent();
-
-
         return root;
 
 
     }
 
     public void setEvent() {
-
-
-        //Gọi dữ liệu lên màn hình
-        customAdapter_banh = new BanhAdapter(getContext(), R.layout.banh_listview, data_banh);
-        lvDanhSach.setAdapter(customAdapter_banh);
-
         //
         if (customAdapter_banh == null) {
             FirebaseBanh.LoadDSBanh(new FirebaseBanh.IListener() {
@@ -86,19 +76,19 @@ public class HomeFragment extends Fragment {
 
         //
         if (customAdapter_nuoc == null) {
-//            FirebaseNuoc.LoadDSNuoc(new FirebaseNuoc.IListener() {
-//                @Override
-//                public void onSuccess() {
-//                    data_nuoc.addAll(FirebaseNuoc.getArrNuoc());
-//                    customAdapter_nuoc = new NuocAdapter(getActivity(), R.layout.nuoc_listview, data_nuoc);
-//                    lvDanhSach.setAdapter(customAdapter_nuoc);
-//                }
-//                @Override
-//                public void onFail() {}
-//            });
+            FirebaseNuoc.LoadDSNuoc(new FirebaseNuoc.IListener() {
+               @Override
+              public void onSuccess() {
+                    data_nuoc.addAll(FirebaseNuoc.getArrNuoc());
+                    customAdapter_nuoc = new NuocAdapter(getActivity(), R.layout.nuoc_listview, data_nuoc);
+                    lvDanhSach.setAdapter(customAdapter_nuoc);
+                }
+               @Override
+                public void onFail() {}
+            });
         }
         else {customAdapter_banh.notifyDataSetChanged();
-        }
+       }
 
 
         btnKhu.setOnClickListener(new View.OnClickListener() {
@@ -136,6 +126,90 @@ public class HomeFragment extends Fragment {
         });
 
 
+    }
+
+    // Tạo dữ liệu
+    private void KhoiTao() {
+        //Gọi dữ liệu có sẵn
+        Banh banh = new Banh();
+        banh.setTenBanh("Bánh Sinh Nhật");
+        banh.setGiaCa("250.000đ");
+        banh.setDiaChi("Thiên Ân");
+        banh.setGiam("5%");
+        data_banh.add(banh);
+
+        //Gọi dữ liệu có sẵn
+        Banh banh1 = new Banh();
+        banh1.setTenBanh("Flan");
+        banh1.setGiaCa("10.000đ");
+        banh1.setDiaChi("Thiên Ân");
+        banh1.setGiam("");
+        data_banh.add(banh1);
+
+        //Gọi dữ liệu có sẵn
+        Banh banh2 = new Banh();
+        banh2.setTenBanh("Bánh Ngọt");
+        banh2.setGiaCa("10.000đ");
+        banh2.setDiaChi("Thiên Hương");
+        banh2.setGiam("1%");
+        data_banh.add(banh2);
+
+        //Gọi dữ liệu có sẵn
+        Banh banh3 = new Banh();
+        banh3.setTenBanh("Bánh Kem");
+        banh3.setGiaCa("100.000đ");
+        banh3.setDiaChi("Thiên Ân");
+        banh3.setGiam("2%");
+        data_banh.add(banh3);
+
+        //Gọi dữ liệu có sẵn
+        Banh banh4 = new Banh();
+        banh4.setTenBanh("Bánh Trai Cây");
+        banh4.setGiaCa("30.000đ");
+        banh4.setDiaChi("Thiên Ân");
+        banh4.setGiam("1%");
+        data_banh.add(banh4);
+
+
+        //Gọi dữ liệu có sẵn
+        Nuoc nuoc = new Nuoc();
+        nuoc.setTenNuoc("Coca");
+        nuoc.setGiaCa("400.000đ");
+        nuoc.setDiaChi("Thiên Ân");
+        nuoc.setGiam("1%");
+        data_nuoc.add(nuoc);
+
+        //Gọi dữ liệu có sẵn
+        Nuoc nuoc1 = new Nuoc();
+        nuoc1.setTenNuoc("Coca");
+        nuoc1.setGiaCa("10.000đ");
+        nuoc1.setDiaChi("Thiên Ân");
+        nuoc1.setGiam("1%");
+        data_nuoc.add(nuoc1);
+
+        //Gọi dữ liệu có sẵn
+        Nuoc nuoc2 = new Nuoc();
+        nuoc2.setTenNuoc("Coca");
+        nuoc2.setGiaCa("10.000đ");
+        nuoc2.setDiaChi("Thiên Hương");
+        nuoc2.setGiam("1%");
+        data_nuoc.add(nuoc2);
+
+        //Gọi dữ liệu có sẵn
+        Nuoc nuoc3 = new Nuoc();
+        nuoc3.setTenNuoc("Coca");
+        nuoc3.setGiaCa("10.000đ");
+        nuoc3.setDiaChi("Thiên Ân");
+        nuoc3.setGiam("1%");
+        data_nuoc.add(nuoc3);
+
+        //Gọi dữ liệu có sẵn
+        Nuoc nuoc4 = new Nuoc();
+        nuoc4.setTenNuoc("Coca");
+        nuoc4.setGiaCa("10.000đ");
+        nuoc4.setDiaChi("Thiên Ân");
+        nuoc4.setGiam("1%");
+        data_nuoc.add(nuoc4);
     }
 
     /**private class ItemLongClickRemove implements AdapterView.OnItemLongClickListener {
