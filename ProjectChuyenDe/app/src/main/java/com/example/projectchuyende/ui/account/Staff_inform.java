3d2e515a769d619397650654348b5f5b663a9b33
Trần Minh Thuận <32482005+songtu1307@users.noreply.github.com>
2021-01-14 01:11:13 +0700

    update
    layout

--- ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/account/Staff_inform.java
+++ ProjectChuyenDe/app/src/main/java/com/example/projectchuyende/ui/account/Staff_inform.java
@@ -3,6 +3,7 @@ package com.example.projectchuyende.ui.account;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
@@ -11,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -19,12 +21,13 @@ import com.example.projectchuyende.ui.bill.Bill;
 
 public class Staff_inform extends AppCompatActivity {
     Button btn_taikhoan;
+    ImageView imgBack;
     TextView txtMaNV, txtTenNV, txtChucvu, txtSDT, txtGioitinh, txtEmail, txtLuong;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_inform);
-
+        Toolbar toolbar = findViewById(R.id.toolbarttnv);
         btn_taikhoan = findViewById(R.id.btntaikhoan);
         txtMaNV = findViewById(R.id.txtmanv);
         txtTenNV = findViewById(R.id.txtnamenv);
@@ -33,11 +36,13 @@ public class Staff_inform extends AppCompatActivity {
         txtGioitinh = findViewById(R.id.txtGioitinh);
         txtEmail = findViewById(R.id.txtmail);
         txtLuong = findViewById(R.id.txtLuong);
+        setSupportActionBar(toolbar);
         setEvent();
 
     }
 
     public void setEvent() {
+
         Intent intent=getIntent();
         txtTenNV.setText(intent.getStringExtra("Tennv"));
         txtMaNV.setText(intent.getStringExtra("Manv"));
@@ -46,11 +51,19 @@ public class Staff_inform extends AppCompatActivity {
         txtGioitinh.setText(intent.getStringExtra("gioitinh"));
         txtSDT.setText(intent.getStringExtra("SDT"));
         txtChucvu.setText(intent.getStringExtra("Chucvu"));
+        imgBack= findViewById(R.id.imgBack);
         btn_taikhoan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Staff_inform.this, Account.class);
                 startActivity(intent);
+            }
+        });
+
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
             }
         });
     }
